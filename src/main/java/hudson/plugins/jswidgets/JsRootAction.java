3f865bc336797895156d401ee3682b5756448abe
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2014-06-25 22:32:59 +0200

    RunList is not a real list anymore.

--- src/main/java/hudson/plugins/jswidgets/JsRootAction.java
+++ src/main/java/hudson/plugins/jswidgets/JsRootAction.java
@@ -1,53 +1,57 @@
 package hudson.plugins.jswidgets;
 
 import hudson.Extension;
-import hudson.model.Hudson;
 import hudson.model.RootAction;
+import hudson.model.Run;
 import hudson.util.RunList;
-
+import jenkins.model.Jenkins;
 import org.kohsuke.stapler.StaplerRequest;
+
+import java.util.ArrayList;
 
 /**
  * Implements methods for javascript root widgets.
- * 
+ *
  * @author mfriedenhagen
  */
 @Extension
 public class JsRootAction extends JsBaseAction implements RootAction {
 
-    /** 
+    /**
      * {@inheritDoc}
      *
-     * This actions always starts from the context directly, so prefix {@link JsConsts} with a slash. 
+     * This actions always starts from the context directly, so prefix {@link JsConsts} with a slash.
      */
     @Override
     public String getUrlName() {
         return "/" + JsConsts.URLNAME;
     }
-    
+
     /**
      * Returns some or all known runs of this hudson instance, depending on parameter count.
-     * 
+     *
      * @param request evalutes parameter <tt>count</tt>
      * @return runlist
      */
     @SuppressWarnings({ "rawtypes", "unchecked" })
     public RunList getRunList(StaplerRequest request) {
-        final RunList allRuns = new RunList(Hudson.getInstance().getPrimaryView());
+        final RunList<Run> allRuns = new RunList(Jenkins.getInstance().getPrimaryView());
         final String countParameter = request.getParameter("count");
         if (countParameter == null) {
             return allRuns;
-        } else {            
+        } else {
+            final ArrayList<Run> result = new ArrayList<Run>();
             final int count = Integer.valueOf(countParameter);
-            if (count > allRuns.size()) {
-                return allRuns;
-            } else {
-                final RunList runList = new RunList();
-                for (int i = 0; i < count; i++) {
-                    runList.add(allRuns.get(i));
+            int current = 0;
+            for (final Run run : allRuns) {
+                if (current < count) {
+                    result.add(run);
+                } else {
+                    break;
                 }
-                return runList;
+                current++;
             }
+            return RunList.fromRuns(result);
         }
     }
 
